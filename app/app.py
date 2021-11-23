45a709548dedc97922945d0a66b69041c08428b1
NarayanAdithya <narayanadithya1234@gmail.com>
2021-11-23 16:35:37 +0530

    Fully Dockerized With Fucntioning Backend

--- app/app.py
+++ app/app.py
@@ -1,14 +1,26 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_pymongo import PyMongo
 import os
 
 app = Flask(__name__)
 MONGO_URI = os.environ.get('MONGO_URI')
-db=PyMongo(app,uri=MONGO_URI)
-
+app.config['MONGO_URI']=MONGO_URI+'?authSource=admin'
+mongo_client=PyMongo(app)
+db=mongo_client.db
 @app.route('/')
 def home():
-    return "Hey Hello World"
+    return "Hey Hello World!!!"
+
+@app.route('/add_movie')
+def add_movie():
+    movie_details={'Name':'The Story of My Life','Director':'Adithya','Producer':'Narayan','IMDB_Rating':9.8}
+    db.movie.insert_one(movie_details)
+    return "Movie Added Successfully"
+
+@app.route('/get_movies')
+def get_movies():
+    movie=db.movie.find({})
+    return jsonify([todo for todo in movie])
 
 
 if __name__=='__main__':
